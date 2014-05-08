9bf2487e83057ecab9a453738ccab34766bd841d
dioneeduardo <dione.eduardo@gmail.com>
2014-05-08 20:18:41 -0300

    Master
    Master

---
+++ SisLojaWeb/SisLojaWeb/Login.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SisLojaWeb.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <link href="css/StylePage.css" rel="stylesheet" />
+
+</head>
+<body style="background-color:#1b1b1b;">
+
+
+
+    <form id="form1" runat="server">
+    <div class="BoxUsuario">
+    
+        Identifique-se
+        
+        <br />
+        <br />
+        <br />
+
+        <asp:Label ID="Label1" runat="server" Text="Usuario"></asp:Label>
+        <asp:TextBox ID="txtUsuario" runat="server" CssClass="TextBoxlogin"></asp:TextBox>
+
+        <br>
+
+        <asp:Label ID="Label2" runat="server" Text="Senha"></asp:Label>
+        <asp:TextBox ID="txtSenha" runat="server" CssClass="textboxSenha TextBoxlogin" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnlogin" runat="server" Text="Logar" CssClass="but" OnClick="btnlogin_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ SisLojaWeb/SisLojaWeb/Login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SisLojaWeb
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection con = new SqlConnection();
+            SqlDataReader dr;
+
+            con.ConnectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=SisLoja;Integrated Security=True";
+            cmd.Connection = con;
+            con.Open();
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "ValidaUsuario";
+            cmd.Parameters.Clear();
+
+            cmd.Parameters.AddWithValue("@Login", txtUsuario.Text);
+            cmd.Parameters.AddWithValue("@Senha", txtSenha.Text);
+
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                Response.Write("Ok");
+            }
+            con.Close();
+
+        }
+    }
+}
